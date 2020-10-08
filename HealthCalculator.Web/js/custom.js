8c37ba6559e6b0204cac453b86642502c4cfa35c
gwalvanshi <gwalvanshi@gmail.com>
2020-10-08 18:28:49 +0530

    HTML UI

--- HealthCalculator.Web/js/custom.js
+++ HealthCalculator.Web/js/custom.js
@@ -8,3 +8,10 @@ $('.nextStep a').on('click', function() {
 });
 /* ***************** show / hide div end ******************************** */
 
+/* ***************** show / hide div ******************************** */
+$('.nextStepA a').on('click', function () {
+    var target = $(this).attr('rel');
+    $("." + target).show().siblings("div").hide();
+});
+/* ***************** show / hide div end ******************************** */
+
