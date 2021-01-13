77bede5183e212a4cb559263413af9c3284b3237
gwalvanshi <gwalvanshi@gmail.com>
2021-01-13 09:05:03 +0530

    Payment gatway

--- HealthCalculator.Web/Controllers/PaymentController.cs
+++ HealthCalculator.Web/Controllers/PaymentController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PayPal.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,19 +28,162 @@ namespace HealthCalculator.Web.Controllers
         public string address { get; set; }
         public int amount { get; set; }
     }
-    public class PaymentController: BaseController
+    public class PaymentController : BaseController
     {
+        public ActionResult FailureView()
+        {
+            return View();
+        }
+        public ActionResult SuccessView()
+        {
+            return View();
+        }
+        public ActionResult PaymentWithPaypal(string Cancel = null)
+        {
+            //getting the apiContext  
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+            try
+            {
+                //A resource representing a Payer that funds a payment Payment Method as paypal  
+                //Payer Id will be returned when payment proceeds or click to pay  
+                string payerId = Request.Params["PayerID"];
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //this section will be executed first because PayerID doesn't exist  
+                    //it is returned by the create function call of the payment class  
+                    // Creating a payment  
+                    // baseURL is the url on which paypal sendsback the data.  
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority + "/Payment/PaymentWithPayPal?";
+                    //here we are generating guid for storing the paymentID received in session  
+                    //which will be used in the payment execution  
+                    var guid = Convert.ToString((new Random()).Next(100000));
+                    //CreatePayment function gives us the payment approval url  
+                    //on which payer is redirected for paypal account payment  
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
+                    //get links returned from paypal in response to Create function call  
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be redirected for payment  
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid  
+                    Session.Add(guid, createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This function exectues after receving all parameters for the payment  
+                    var guid = Request.Params["guid"];
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+                    //If executed payment failed then we will show payment failure message to user  
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("FailureView");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("FailureView");
+            }
+            //on successful payment, show success page to user.  
+            return View("SuccessView");
+        }
+        private PayPal.Api.Payment payment;
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            this.payment = new Payment()
+            {
+                id = paymentId
+            };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
+        {
+            //create itemlist and add item objects to it  
+            var itemList = new ItemList()
+            {
+                items = new List<Item>()
+            };
+            //Adding Item Details like name, currency, price etc  
+            itemList.items.Add(new Item()
+            {
+                name = "Item Name comes here",
+                currency = "USD",
+                price = "1",
+                quantity = "1",
+                sku = "sku"
+            });
+            var payer = new Payer()
+            {
+                payment_method = "paypal"
+            };
+            // Configure Redirect Urls here with RedirectUrls object  
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl + "&Cancel=true",
+                return_url = redirectUrl
+            };
+            // Adding Tax, shipping and Subtotal details  
+            var details = new Details()
+            {
+                tax = "1",
+                shipping = "1",
+                subtotal = "1"
+            };
+            //Final amount with details  
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = "3", // Total must be equal to sum of tax, shipping and subtotal.  
+                details = details
+            };
+            var transactionList = new List<Transaction>();
+            // Adding description about the transaction  
+            transactionList.Add(new Transaction()
+            {
+                description = "Transaction description",
+                invoice_number = "your generated invoice number", //Generate an Invoice No  
+                amount = amount,
+                item_list = itemList
+            });
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+            // Create a payment using a APIContext  
+            return this.payment.Create(apiContext);
+        }
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult PaymentPage()
+        {
+            return View();
+        }
+        
         [HttpPost]
         public ActionResult CreateOrder(PaymentInitiateModel _requestData)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             // Generate random receipt number for order
             Random randomObj = new Random();
             string transactionId = randomObj.Next(10000000, 100000000).ToString();
-            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_umbrFAbJ3slyJ", "su9eXFaihGucmKECVRcRk0Q");
+            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_dcqe3aqpMbfHdP", "f20iAuCr0Rz9ZE2dSvccDesd");
             Dictionary<string, object> options = new Dictionary<string, object>();
             options.Add("amount", _requestData.amount * 100);  // Amount will in paise
             options.Add("receipt", transactionId);
@@ -51,7 +196,7 @@ namespace HealthCalculator.Web.Controllers
             OrderModel orderModel = new OrderModel
             {
                 orderId = orderResponse.Attributes["id"],
-                razorpayKey = "rzp_test_umbrFAbVJ3slyJ",
+                razorpayKey = "rzp_test_dcqe3aqpMbfHdP",
                 amount = _requestData.amount * 100,
                 currency = "INR",
                 name = _requestData.name,
@@ -63,7 +208,7 @@ namespace HealthCalculator.Web.Controllers
             // Return on PaymentPage with Order data
             return View("PaymentPage", orderModel);
         }
-      
+
         [HttpPost]
         public ActionResult Complete()
         {
@@ -72,7 +217,7 @@ namespace HealthCalculator.Web.Controllers
             string paymentId = Request.Params["rzp_paymentid"];
             // This is orderId
             string orderId = Request.Params["rzp_orderid"];
-            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_umbrFAbVJ3lyJ", "su9eXFaihGucMmKEVRcRk0Q");
+            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_dcqe3aqpMbfHdP", "f20iAuCr0Rz9ZE2dSvccDesd");
             Razorpay.Api.Payment payment = client.Payment.Fetch(paymentId);
             // This code is for capture the payment 
             Dictionary<string, object> options = new Dictionary<string, object>();
